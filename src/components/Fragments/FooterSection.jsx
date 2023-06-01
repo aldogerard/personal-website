89beb4ab8edf831c5b97935529aa143f47d8bd72
aldogerard <aldogerard12@gmail.com>
2023-06-02 00:55:50 +0700

    fix margin top footer

--- src/components/Fragments/FooterSection.jsx
+++ src/components/Fragments/FooterSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const FooterSection = () => {
   return (
     <>
-      <div className="mt-4 w-full py-10">
+      <div className="mt-12 w-full py-10">
         <h6 className="text-center text-xs text-gray-500 dark:text-gray-400 md:text-sm">© Reynaldo Gerard. All rights reserved</h6>
       </div>
     </>
