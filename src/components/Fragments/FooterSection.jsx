9f18d407bc7142f005a5328caad3a8e48f5b7a39
Reynaldo Gerard Tuerah <aldogerard12@gmail.com>
2024-09-30 18:02:01 +0700

    feat : add year on footer

--- src/components/Fragments/FooterSection.jsx
+++ src/components/Fragments/FooterSection.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 const FooterSection = () => {
-  return (
-    <>
-      <div className="mt-12 w-full py-10">
-        <h6 className="text-center text-xs text-gray-500 dark:text-gray-400 md:text-sm">© Reynaldo Gerard. All rights reserved</h6>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="mt-12 w-full py-10">
+                <h6 className="text-center text-xs text-gray-500 dark:text-gray-400 md:text-sm">
+                    © 2023 Reynaldo Gerard. All rights reserved
+                </h6>
+            </div>
+        </>
+    );
 };
 
 export default FooterSection;
