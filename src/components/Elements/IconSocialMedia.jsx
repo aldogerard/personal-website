04e3b4e12693e0d70e7b4ceb8106a87c796436f8
aldogerard <111220141+aldogerard@users.noreply.github.com>
2025-05-02 03:38:21 +0700

    Update IconSocialMedia.jsx

--- src/components/Elements/IconSocialMedia.jsx
+++ src/components/Elements/IconSocialMedia.jsx
@@ -10,19 +10,19 @@ const IconSocialMedia = () => {
     return (
         <>
             <div className="mt-6 flex gap-4 md:mt-8">
-                <a href="https://www.instagram.com/aldogerard_" target="_blank">
+                <a href="https://www.instagram.com" target="_blank">
                     <IoLogoInstagram className="icon" />
                 </a>
-                <a href="https://www.github.com/aldogerard" target="_blank">
+                <a href="https://www.github.com" target="_blank">
                     <IoLogoGithub className="icon" />
                 </a>
                 <a
-                    href="https://www.linkedin.com/in/reynaldogerard"
+                    href="https://www.linkedin.com/in/"
                     target="_blank"
                 >
                     <IoLogoLinkedin className="icon" />
                 </a>
-                <a href="https://wa.me/6281259488643" target="_blank">
+                <a href="https://wa.me/" target="_blank">
                     <IoLogoWhatsapp className="icon" />
                 </a>
             </div>
